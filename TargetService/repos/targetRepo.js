3e586f502ccab3cbd5a828c8a4f104248a5489e5
Quinten M <quinten@infi.nl>
2023-03-14 11:55:51 +0100

    No more duplicates possible. Pre-save validation.

--- TargetService/repos/targetRepo.js
+++ TargetService/repos/targetRepo.js
@@ -1,14 +1,18 @@
 const Target = require('../models/target');
+const publisher = require("../rabbitMQ/publisher");
 
-function putMessage(username, location, base64) {
+/**
+    *  Saves a user to the database
+    *  @async
+    *  @param {string} username - The username of the user to save
+    *  @returns {Promise<Target>} - The saved user
+    */
+    async function saveUser(username) {
+    const target = new Target({
+        username: username,
+    })
+
     return new Promise((resolve, reject) => {
-
-        const target = new Target({
-            base64: base64,
-            username: username,
-            location: location
-        })
-
         target.save()
             .then(savedValue => {
                 if (savedValue) {
@@ -20,9 +24,37 @@ function putMessage(username, location, base64) {
                 reject(e);
             })
     })
+    }
+
+/**
+    *  Checks if a user exists in the database. If exists, returns the user object.
+    *  If not, returns false
+    *  @async
+    *  @param {string} username - The username to check
+    *  @returns {Promise<Object|boolean>} - Returns the user object if found, false if not found
+    */
+async function userExists(username){
+    try {
+        return await Target.findOne({username: username});
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-function deleteMessage(value) {
+async function retrieveTarget(username){
+    return new Promise((resolve, reject) => {
+        Target.findOne({ username: username }).then((target) => {
+            if (target) {
+                resolve(target)
+            }
+            reject(false)
+        }).catch((e) => {
+            reject('Something went wrong. Error: ' + e)
+        });
+    })
+}
+
+async function deleteTarget(value) {
     return new Promise((resolve, reject) => {
         Target.deleteOne({ _id: value })
             .then((ok) => {
@@ -36,7 +68,65 @@ function deleteMessage(value) {
     })
 }
 
+
+async function saveUserTarget(user, target) {
+    const {image, location} = target;
+
+    return new Promise((resolve, reject) => {
+        const binaryData = Buffer.from(image, 'utf8');
+        const base64EncodedData = binaryData.toString('base64');
+
+        user.targets.push({
+            base64: base64EncodedData,
+            location: location
+        });
+
+        user.save()
+            .then((savedValue) => {
+                if (savedValue) {
+                    resolve(savedValue)
+                }
+            })
+            .catch(e => {
+                console.trace('Saving failed @ saveUserTarget')
+                reject(e);
+            })
+    })
+
+    // const payload = createPayload(
+    //     'create',
+    //     `${answer.id}`,
+    //     "user",
+    //     {
+    //         uploadByUsername: answer.uploadByUsername,
+    //         base64: answer.base64,
+    //         location: answer.location
+    //     },
+    // );
+    // publisher(payload);
+
+}
+
+
+async function checkForExistingImage(image) {
+    return new Promise((resolve, reject) => {
+        Target.findOne({ 'targets.base64': image })
+            .then((target) => {
+                if (target) {
+                    resolve(target)
+                }
+                reject(false)
+            }).catch((e) => {
+            reject('Something went wrong. Error: ' + e)
+        });
+    })
+}
+
 module.exports = {
-    putMessage,
-    deleteMessage
+    deleteTarget,
+    userExists,
+    saveUserTarget,
+    saveUser,
+    retrieveTarget,
+    checkForExistingImage
 }
--- TargetService/routes/index.js
+++ TargetService/routes/index.js
@@ -1,4 +1,4 @@
-const {deleteMessage, putMessage} = require('../repos/targetRepo')
+const {deleteTarget, saveUser, userExists, saveUserTarget, retrieveTarget} = require('../repos/targetRepo')
 
 const express = require('express');
 const router = express.Router();
@@ -7,7 +7,29 @@ const {createPayload} = require("../payloadHandling/payloadCreator");
 const publisher = require("../rabbitMQ/publisher");
 const createError = require("http-errors");
 const multer = require('multer')
+
 const upload = multer();
+
+
+
+router.post('/target/test',
+    async (req, res, next) => {
+    const saveuser = await saveUser('test').catch((e) => { console.log(e) });
+
+    // const user = await retrieveTarget('test')
+    //     .catch((e) => { console.log(e) });
+
+    console.log(saveuser)
+
+    saveuser.targets.push({
+        location: 'test',
+        base64: 'test'
+    })
+    saveuser.save().catch((e) => { console.log(e) });
+
+    console.log(saveuser)
+    res.status(200).send('OK');
+});
 
 const targetUpload = upload.fields([
     {name: 'image', maxCount: 1},
@@ -17,53 +39,50 @@ const targetUpload = upload.fields([
 router.post('/target',
     targetUpload,
     async (req, res, next) => {
-    const data = JSON.parse(req.body.target);
-    const image = req.files.image[0]['buffer'];
+        const data = JSON.parse(req.body.target);
+        const image = req.files.image[0]['buffer'];
+        const {username, location} = data;
 
-    const {username, location} = data;
+        if (!username || !location || !image) {
+            return next(createError(400, 'Missing parameters'))
+        }
 
-    if (!username || !location || !image) {
-        return next(createError(400, 'Missing parameters'))
-    }
-
-    const binaryData = Buffer.from(image, 'utf8');
-    const base64EncodedData = binaryData.toString('base64');
-
-    await putMessage(username, location, base64EncodedData)
-        .then(answer => {
-            res.status(202).send(`Target: (${answer.id}) is aangemaakt.`);
-            return answer;
+        await userExists(username).then((user) => {
+            if (user !== null && user !== undefined) {
+                saveUserTarget(user, {location, image})
+                    .catch((e) => { console.log(e) });
+            } else {
+                // User does not exist, create user and save target to user
+                saveUser(username)
+                    .then((user) => { saveUserTarget(user, {location, image})
+                    })
+                    .catch((e) => { console.log(e) });
+            }
         })
-        .then(answer => {
-            const payload = createPayload(
-                'create',
-                `${answer.id}`,
-                "user",
-                {
-                    uploadByUsername: answer.uploadByUsername,
-                    base64: answer.base64,
-                    location: answer.location
-                },
-            );
-            publisher(payload);
-        })
-        .catch(err => {
-            console.log("Saving to database failed or target already exists: " + err);
-            next(new Error('Saving to database failed or target already exists.'));
-        });
-})
+        res.status(200).send('OK');
+    })
+
+router.get('/target/:id',
+    async (req, res, next) => {
+        const id = req.params.id;
+
+        if (!id) {
+            next(new Error('Incorrect format.'));
+        }
+
+        res.render('image', {id: '10', username: 'quinten', location: 'ams'});
+    })
 
 router.delete('/target/:id', async (req,
                                     res,
                                     next) => {
-
     const id = req.params.id;
 
     if (!id) {
         next(new Error('Incorrect format.'));
     }
 
-    await deleteMessage(id).then((m) => {
+    await deleteTarget(id).then((m) => {
         if (m.code === 0) {
             console.log(`${m.message}`);
             res.send(m.message);
@@ -73,8 +92,7 @@ router.delete('/target/:id', async (req,
         console.trace(`Deleting target ${id} failed: ${err}`);
     });
 
-    // action,id,fieldName,dataValue
-    const payloadCreator = new PayloadCreator('delete', `${id}`, "user", {});
+    const payloadCreator = createPayload('delete', `${id}`, "user", {});
     await publisher(payloadCreator.getPayload())
         .catch(err => {
             console.trace("Sending delete message to rabbitMQ failed: " + err);
