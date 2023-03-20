e0a7379e72d2150c70c61ad907160b4971c7a421
lucasvanrooij19@gmail.com <lucasvanrooij19@gmail.com>
2023-03-20 23:10:19 +0100

    small fixes

--- config/interceptor.js
+++ config/interceptor.js
@@ -1,13 +1,14 @@
 var jwt = require('jsonwebtoken');
+const api = process.env.API_KEY;
 
 const interceptor = function (request) {
     const id = request.headers['UserId'];
     if (id !== undefined) {
-        const token = jwt.sign({ process.env.API_KEY, id }, process.env.JWT_SECRET);
+        const token = jwt.sign({ api, id }, process.env.JWT_SECRET);
         request.headers.Authorization = `Bearer ${token}`;
         delete request.headers['UserId'];
     } else {
-        const token = jwt.sign({ process.env.API_KEY }, process.env.JWT_SECRET);
+        const token = jwt.sign({ api }, process.env.JWT_SECRET);
         request.headers.Authorization = `Bearer ${token}`;
     }
     return request;
