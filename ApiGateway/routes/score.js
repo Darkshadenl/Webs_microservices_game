3948693e8626c4d9a85cc3c4e8c68cba63c8d571
Quinten M <quinten@infi.nl>
2023-04-05 21:24:48 +0200

    gateway image upload route

--- ApiGateway/routes/score.js
+++ ApiGateway/routes/score.js
@@ -7,7 +7,7 @@ const {options: jwtOptions} = require("../../config/passportStrategy");
 const scoreService    =  process.env.SCOREURL || 'http://localhost:3000/'
 const messageSender = require('../helpers/messageSender')
 const roles = require('../helpers/authorizationRole');
-
+const multer = require('multer');
 const strategy = require('../helpers/PasportStrategy')
 
 passport.use(strategy);
@@ -19,7 +19,20 @@ const circuitBreaker = require('../helpers/circuitBreaker')
 
 router.get('/test',roles('admin'), messageSender(circuitBreaker,'get','test'));
 router.get('/', messageSender(circuitBreaker,'get',''));
+router.post('', messageSender(circuitBreaker,'post','score'))
 
+const upload = multer({
+    storage: multer.memoryStorage(),
+    limits: { fileSize: 5000000 } // optional file size limit
+});
+router.post('/convertImage', upload.single('image'), (req, res) => {
+    const file = req.file; // the uploaded file
+    if (!file) {
+        return res.status(400).send({ message: 'No file uploaded' });
+    }
+    const base64String = file.buffer.toString('base64');
+    res.send(base64String);
+})
 
 
 module.exports = router
