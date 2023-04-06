8bef8eea0f89cd7789721201c241f000dffa2659
lucasvanrooij19@gmail.com <lucasvanrooij19@gmail.com>
2023-04-06 17:01:58 +0200

    remove unused passport

--- ApiGateway/routes/score.js
+++ ApiGateway/routes/score.js
@@ -1,17 +1,11 @@
 const express = require('express');
 const router = new express.Router();
-const passport = require('passport');
-const axios = require('axios');
-const {Strategy: JwtStrategy} = require("passport-jwt");
-const {options: jwtOptions} = require("../../config/passportStrategy");
 const scoreService    =  process.env.SCOREURL || 'http://localhost:3000/'
 const messageSender = require('../helpers/messageSender')
 const roles = require('../helpers/authorizationRole');
 const multer = require('multer');
-const strategy = require('../helpers/PasportStrategy')
 
-passport.use(strategy);
-router.use(passport.initialize());
+
 
 const circuitBreaker = require('../helpers/circuitBreaker')
     .createNewCircuitBreaker(scoreService);
--- ApiGateway/routes/target.js
+++ ApiGateway/routes/target.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = new express.Router();
-const passport = require('passport');
-const axios = require('axios');
-const {Strategy: JwtStrategy} = require("passport-jwt");
-const {options: jwtOptions} = require("../../config/passportStrategy");
 const targetService    =  process.env.TARGETURL || 'http://localhost:3000/'
 const messageSender = require('../helpers/messageSender')
 
-const strategy = require('../helpers/PasportStrategy')
-
-passport.use(strategy);
-router.use(passport.initialize());
 
 const circuitBreaker = require('../helpers/circuitBreaker')
     .createNewCircuitBreaker(targetService);
